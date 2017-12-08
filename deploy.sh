88fd0de503d6ed4d3d9b7d6b452e0148bf06c568
Dmitri Sivachenko <d.sivachenko@hotmail.com>
2017-12-08 03:19:00 +0300

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#! /user/env sh
+
+set -e
+zip -r build.zip build
+
+curl  -H "Content-type: application/zip" \
+      -H "Authorization: Bearer $NETLIFY_KEY" \
+      --data-binary "@build.zip" \
+      https://api.netlify.com/api/v1/sites
